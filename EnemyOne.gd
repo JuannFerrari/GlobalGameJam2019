0199d4446197c190555499bf065969d85c841875
Juan Ferrari <juannferrari@gmail.com>
2019-01-26 00:01:32 -0300

    song added, other fixes

--- EnemyOne.gd
+++ EnemyOne.gd
@@ -1,13 +1,12 @@
 extends KinematicBody2D
 
-var zumbie = self
-var speed = 2
-onready var target = get_parent().get_node("PlayerOne")
+var speed = 100
+var direction = 0
+onready var target = get_parent().get_node("Player")
 
 func _ready():
 	pass
 
-func _fixed_process(delta):
-	var direction = (target.get_global_pos() - zumbie.get_global_pos()).normalized()
-	print(direction)
-	move(direction*speed) 
+func _physics_process(delta):
+	direction = (target.position - position).normalized()
+	move_and_slide(direction*speed) 
