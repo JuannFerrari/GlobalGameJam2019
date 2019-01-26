5c9c970b55e7531c10c4e8b523be33397f78b725
matiascondem <matiascondemartinez1997@gmail.com>
2019-01-26 09:38:45 -0300

    balas ya matan los bichos

--- EnemyOne.gd
+++ EnemyOne.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var speed = 100
 var direction = 0
+var life=1
 onready var target = get_parent().get_node("Player")
 var wr = weakref(self);
 
@@ -11,6 +12,7 @@ func _ready():
 func _physics_process(delta):
 	direction = (target.position - position).normalized()
 	var collision_info = move_and_collide(direction*speed * delta) 
-	
+	if life <=0:
+		queue_free()
 	#	if collision_info:   #only collides after move
 	#		collision_info.collider.queue_free()
