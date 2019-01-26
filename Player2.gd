510d57ddc835d34c0fc346dbc8de20cb198f4e90
Cesarb91 <47044773+Cesarb91@users.noreply.github.com>
2019-01-26 20:07:14 -0300

    Revert "Merge branch 'master' of https://github.com/Valatir/GlobalGameJam2019"
    This reverts commit c2afb4344af08a8aecdae579d380e34ed685d778, reversing
    changes made to 3db245ce4ee43f5770f672e79ef57760f9530512.

--- Player2.gd
+++ Player2.gd
@@ -31,7 +31,6 @@ func _physics_process(delta):
 
 	if !dead:
 		if Input.is_action_just_pressed(action_shoot):
-			$AnimatedSprite.play("attack")
 			shoot_bullet()
 
 		if Input.is_action_pressed(action_right):
@@ -49,20 +48,19 @@ func _physics_process(delta):
 			motion.y=lerp(motion.y,0,0.2)
 
 		direction=motion.angle()
-		
-		if (motion.x<5 and motion.x >-5) and (motion.y<5 and motion.y >-5):
-			$AnimatedSprite.play("idle")
+
+		if(motion < Vector2(1, 1) and motion > Vector2(-1, -1) ):
+			if $AnimationPlayer.current_animation != "idle":
+				$AnimationPlayer.play("idle")
 		else:
-			$AnimatedSprite.play("walk")
-		
-		if motion.x >0:
-			$AnimatedSprite.flip_h=true
-		else:
-			$AnimatedSprite.flip_h=false
+			if ($AnimationPlayer.current_animation != "Move"):
+				$AnimationPlayer.play("Move")
+
 		move_and_collide(motion * delta)
 
 
 	else:
+		$AnimationPlayer.play("idle")
 		$CollisionShape2D.disabled = true
 
 
@@ -86,7 +84,6 @@ func take_damage():
 	if life<=0:
 		dead = true
 		#is now ded blep
-		$AnimatedSprite.play("idle")
 		set_rotation_degrees(85)
 
 func _on_Hitbox_area_shape_entered(area_id, area, area_shape, self_shape):
